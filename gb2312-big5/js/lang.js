ef185683de46c9e3ea597b4e877ef0bf9481c066
SteakOverCooked <Oh@SteakOverCooked.com>
2018-01-29 00:52:15 +0000

    pinyin

--- gb2312-big5/js/lang.js
+++ gb2312-big5/js/lang.js
@@ -9,9 +9,9 @@ var app_name = manifest.name + " v" + manifest.version;
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('msg').innerHTML = 'App: ' + app_name;
 	$('select#setting').change(function() {
-	  chrome.storage.sync.set({ setting: document.getElementById("setting").selectedIndex });
+		chrome.storage.sync.set({ setting: document.getElementById("setting").selectedIndex });
 	});   
 	chrome.storage.sync.get('setting', function(data) {
-	  document.getElementById("setting").selectedIndex = data.setting;  
+		document.getElementById("setting").selectedIndex = data.setting;  
 	});  
  }, false);
